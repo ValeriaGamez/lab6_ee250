4848de31f347c5a8fa780ef682bc80d5a493b983
Valeria Gamez <vgamez@usc.edu>
2024-03-07 15:30:00 -0800

    more

--- grovepi_sensors.py
+++ grovepi_sensors.py
@@ -13,32 +13,29 @@ grovepi.pinMode(potentiometer,"INPUT")
 # clear lcd screen  before starting main loop
 setText("")
 
-lcd = Grove_LCD()
-
 while True:
-    try:
-        # Read distance value from Ultrasonic Ranger
-        distance = grovepi.ultrasonicRead(ultrasonic_ranger)
-        # Read threshold value from potentiometer
-        threshold = grovepi.analogRead(potentiometer)
+  try:
+    # TODO:read distance value from Ultrasonic Ranger and print distance on LCD
+    distance = grovepi.ultrasonicRead(ultrasonic_ranger)
+    
+    
 
-        # Determine whether object is within threshold distance
-        if distance < threshold:
-            # Print top line with threshold and "OBJ PRES"
-            setRGB(255, 0, 0)  # Set text color to red
-            setText_norefresh("{:.2f} OBJ PRES".format(threshold))
-        else:
-            # Print top line with just threshold
-            setRGB(0, 255, 0)  # Set text color to green
-            setText_norefresh("{:.2f}".format(threshold))
+    # TODO: read threshold from potentiometer
+    threshold = grovepi.analogRead(potentiometer)
+    setText_norefresh(threshold)
+    # print(threshold)
+    # print(distance)
+    
+    # TODO: format LCD text according to threshhold
+    # setText_norefresh(threshold)
 
-        # Print bottom line with current ultrasonic ranger output
-        setRGB(0, 0, 255)  # Set text color to blue
-        lcd.setCursor(1, 0)  # Move cursor to the beginning of the second line
-        setText_norefresh("{:.2f}".format(distance))
+    # if distance < threshold:
+    #   setText(threshold, "cm OBJ PRES")
+    
 
-        # Wait for a while before updating the LCD again
-        time.sleep(0.5)
 
-    except IOError:
-        print("Error")
+    
+  
+    
+  except IOError:
+    print("Error")
